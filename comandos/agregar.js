a8c226a9262130c42baf8898ce9306d3f40efcf0
Matias <142049529+matgithubulp@users.noreply.github.com>
2025-02-23 14:26:22 -0300

    ComandoAgregar.js
    Aun en proceso

---
+++ comandos/agregar.js
@@ -0,0 +1,25 @@
+let i = 1; // se va a utilizar como generador de identificador único (ID)
+const fileSystem = require('fs');
+
+function agregarGasto(descripcion, gasto, categoria, fecha) {
+    if (typeof(descripcion) !== 'string' || typeof(gasto) !== 'number' || typeof(categoria) !== 'string' || !(fecha instanceof Date)) {
+        console.log("Error en el tipo de dato");
+        return;
+    }
+
+    const newGasto = {
+        "ID": i,
+        "Descripcion": descripcion,
+        "Categoria": categoria,
+        "Gasto": gasto,
+        "Fecha": fecha
+    };
+    i++;
+
+    const format = JSON.stringify(newGasto);
+    fileSystem.writeFile("./datos/gastos.json", format, (error) => {
+        if (error) throw error;
+        console.log("Información recibida");
+    });
+}
+
